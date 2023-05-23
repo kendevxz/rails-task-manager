d5da2736ad537e45381ad683c55b64e7d2f7ef05
kenny devin <kendevxz@gmail.com>
2023-05-23 15:52:44 +0800

    Challenge Cleared

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'tasks#index'
+  # get 'tasks', to: 'tasks#index'
+
+  # get '/tasks/new', to: 'tasks#new'
+  # post '/tasks', to: 'tasks#create'
+
+  # get '/tasks/:id', to: 'tasks#show', as: :task
+
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: :tasks_edit
+  # patch '/tasks/:id', to: 'tasks#update'
+
+  # delete '/tasks/:id', to: 'tasks#destroy', as: :delete_task
+
+  resources :tasks, only: [:index, :show, :new, :create, :edit, :destroy, :update]
+
 end
